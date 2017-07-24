7564b4ddaa031ad64fdf8b49b2d2e6c362d610af
Reinaldo Chaves <reichaves@gmail.com>
2017-07-24 16:21:50 -0300

    Added markdown and commands with

--- cria_csv.py
+++ cria_csv.py
@@ -11,10 +11,14 @@ for municipio in brasil:
 		total[estado] = 0
 	total[estado] = total[estado] + habitantes
 
-arquivo = open('habitantes.csv', mode = 'w', encoding = 'utf-8')
-resultado = csv.DictWriter(arquivo, fieldnames = ['estado', 'habitantes'])
-resultado.writeheader()
+# I use below a context manager. It's the word "with" in python. What this syntax does, in the case of files is to execute the close () method on the object as soon as the code exits the indentation. Tip by Guilherme Marthe
 
-for estado, habitantes in total.items():
-        resultado.writerow({'estado': estado, 'habitantes': habitantes})
+with open('habitantes.csv', mode = 'w', encoding = 'utf-8') as arquivo:
+        resultado = csv.DictWriter(arquivo, fieldnames = ['estado', 'habitantes'])
+        resultado.writeheader()
+
+        for estado, habitantes in total.items():
+                resultado.writerow({'estado': estado, 'habitantes': habitantes})
 	
+# If you run your code from REPL (IDLE, ipython, or python itself) the file will not close and the changes will not be 'flushed' to the file.
+# So just add the line arquivo.close() at the end of your script and the changes will be written to your file.
